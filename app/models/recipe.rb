4469ef75152bcbf1afbb75cbb631cf3e9158ddf7
Richard Burd <richardburdx@yahoo.com>
2017-12-18 00:29:34 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 #Placeholder for a model
-class Recipe < ActiveRecord::Base 
-
-end 
+class Recipe < ActiveRecord::Base
+  # Right no I don't see a need for a <<has_many>> or <<belongs_to>> bloc.
+end
