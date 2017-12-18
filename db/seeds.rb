e3026ee881655b62c3ce4cb9780cca8cb1f2dd22
Richard Burd <richardburdx@yahoo.com>
2017-12-18 00:23:17 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
 # remember that in order to create a migration, you gotta start all this out with this command:
 
 # rake db:create_migration NAME=create_users
+
+=begin 
+Pirate.create(name: "Redbeard", height: "tall", weight: "heavy")
+Pirate.create(name: "Blackbeard", height: "normal", weight: "normal")
+Pirate.create(name: "CPT Morgan", height: "short", weight: "light")
+Pirate.create(name: "CPT Sparrow", height: "short", weight: "light")
+=end 
